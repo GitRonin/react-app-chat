1b4ead3c318e037eb88e86a961b0a794317059ac
Nikita <mixggua@gmail.com>
2020-08-19 18:57:41 +0300

    added new Messages, data and like v 1.0

--- src/components/message-input/message-input.js
+++ src/components/message-input/message-input.js
@@ -1,20 +1,28 @@
 import React, {Component} from 'react';
+//import MessageList from '../message-list/message-list';
 import './message-input.css';
-import styled from 'styled-components'
 
-const Title = styled.div`
-    width: 500px;
-`;
 export default class MessageInput extends Component {
+    // this.onSubmit = this.onSubmit.bind(this);
+    
+    // onSubmit(e) {
+    //     e.preventDefault();
+    // }
     render() {
         return (
-                <Title>
+                <>
+                    <form className="bottom-panel d-flex">
+                    <input
+                        type="text"
+                        placeholder="Message"
+                        className="form-control new-post-label"/>
                     <button
                         type="submit"
                         className="btn- btn-outline-secondary">
                         Send
                     </button>
-                </Title>
+                    </form> 
+                </>
         )
     }
 }
--- src/components/message-list/message-list.js
+++ src/components/message-list/message-list.js
@@ -1,12 +1,15 @@
-import React, {Component} from 'react';
+import React from 'react';
 import styled from 'styled-components';
+import { ListGroup } from 'reactstrap';
 import './message-list.css';
 import botAvatar from './botAvatar.svg';
 
 const Title = styled.div`
-    border: 1px solid;
+    border: 2px solid;
     overflow: scroll;
-    max-height: 400px;
+    max-height: 360px;
+    border-radius: 3px;
+    position: relative;
 `;
 const TitleContainer = styled.div`
     width: 500px;
@@ -16,27 +19,54 @@ const TitleContainer = styled.div`
     background-color: #E6E6E6;
     display: flex;
     padding: 0px 10px 0px 0px;
+    float: right;
 `;
 
-const TextСontainer = () => {
-    return(
-        <TitleContainer>
-            <img className="botAvatar" src={botAvatar} alt="Bot Avatar"/>
-            <p id="valueText">Ladyship it daughter securing procured or am moreover mr. Put sir she exercise vicinity cheerful wondered. Continual say suspicion provision you.</p>
-        </TitleContainer>
-    )
-};
-export default class MessageList extends Component {
-    render() {
+
+
+const MessageList = () => {
+    const ArrayMessages = [
+        {id: 1, avatar: "", label: "My chat", data: "14:00", like: false},
+        {id: 2, avatar: "", label: "participants", data: "15:00", like: false},
+        {id: 3, avatar: "", label: "messages", data: "16:00", like: true},
+        {id: 4, avatar: "", label: "Last messager", data: "17:00", like: false},
+        {id: 5, avatar: "", label: "12321421", data: "18:00", like: false}
+    ];
+
+const onToggleLiked = (item, id) => {
+    // if (like) classNames += ' like';
+    if (id === item.id) {
+        if(item.like) {
+            item.like = false;
+        }
+        else{
+            item.like = true;
+        }
+    }
+    
+}
+
+    const elements = ArrayMessages.map((item, id) => {
+        id++;
+        return  (
+            <>
+                <TitleContainer className={item.like ? 'heart' : ''} id="message" onClick={() => onToggleLiked(item, id)}>
+                        <img className="botAvatar" src={botAvatar} alt="Bot Avatar"/>
+                        <p className="valueText" id="valueText">{item.label}</p>
+                        <p className="MessageData">{item.data}</p>
+                </TitleContainer>
+            </>
+        )
+    });
         return(
             <Title>
-                    <TextСontainer/>
-                    <TextСontainer/>
-                    <TextСontainer/>
-                    <TextСontainer/>
-                    <TextСontainer/>
-                    <TextСontainer/>
+                <ListGroup>
+                    {elements}
+                </ListGroup>
             </Title>
         )   
     }
-};
+
+    
+
+export default MessageList;
